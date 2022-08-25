277a318ce3002cb5bbbaa1faaef678eb3a9b04a3
Oleg Savelos <olegsavelos@gmail.com>
2022-08-25 13:42:13 +0300

    Added client interface.

---
+++ Gizmo.Client.Shared/Interfaces/IGizmoClient.cs
@@ -0,0 +1,19 @@
+﻿using Gizmo.Shared.Client.UI.ViewModels;
+using System.Collections.Generic;
+
+namespace Gizmo.Client
+{
+    /// <summary>
+    /// Gizmo client interface.
+    /// </summary>
+    public interface IGizmoClient
+    {
+        public IEnumerable<ProductGroupViewModel> GetProductGroups();
+
+        public IEnumerable<ProductViewModel> GetProducts();
+
+        public IEnumerable<ApplicationViewModel> GetApplications();
+
+        public IEnumerable<ExecutableViewModel> GetExecutables();
+    }
+}
