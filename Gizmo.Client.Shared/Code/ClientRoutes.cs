fcf1f09a6cea01f3d1cfaf7bb8c51e789a0dce02
klapsis-netprojects <klapsis@netprojects.gr>
2022-11-25 17:09:58 +0200

    Updates.

---
+++ Gizmo.Client.Shared/Code/ClientRoutes.cs
@@ -0,0 +1,28 @@
+﻿namespace Gizmo.Client
+{
+    public class ClientRoutes
+    {
+        public const string LoginRoute = "/";
+
+        public const string PasswordRecoveryRoute = "/passwordrecovery";
+        public const string PasswordRecoveryConfirmationRoute = "/passwordrecoveryconfirmation";
+        public const string PasswordRecoverySetNewPasswordRoute = "/passwordrecoverysetnewpassword";
+
+        public const string RegistrationConfirmationMethodRoute = "/registrationconfirmationmethod";
+        public const string RegistrationConfirmationRoute = "/registrationconfirmation";
+        public const string RegistrationBasicFieldsRoute = "/registrationbasicfields";
+        public const string RegistrationAdditionalFieldsRoute = "/registrationadditionalfields";
+
+        public const string HomeRoute = "/home";
+        public const string ApplicationsRoute = "/apps";
+        public const string ApplicationDetailsRoute = "/appdetails/{ApplicationId:int}";
+        public const string ShopRoute = "/shop";
+        public const string ProductDetailsRoute = "/productdetails/{ProductId:int}";
+
+        public const string UserProfileRoute = "/profile";
+        public const string UserDepositsRoute = "/profile/deposits";
+        public const string UserProductsRoute = "/profile/products";
+        public const string UserPurchasesRoute = "/profile/purchases";
+        public const string UserSettingsRoute = "/settings";
+    }
+}
