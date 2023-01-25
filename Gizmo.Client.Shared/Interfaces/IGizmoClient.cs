c8c8ebe691e11d0ae4235ff9527fc1a16997680e
klapsis-netprojects <klapsis@netprojects.gr>
2023-01-25 16:59:29 +0200

    Updates.

--- Gizmo.Client.Shared/Interfaces/IGizmoClient.cs
+++ Gizmo.Client.Shared/Interfaces/IGizmoClient.cs
@@ -7,6 +7,28 @@ namespace Gizmo.Client
     /// </summary>
     public interface IGizmoClient
     {
+        #region Registration
+
+        public Task<bool> AccountCreationByEmailStartAsync(string email);
+
+        public Task<bool> AccountCreationByMobilePhoneStartAsync(string mobilePhone);
+
+        #endregion
+
+        #region Password Recovery
+
+        public Task<bool> UserRecoveryMethodGetAsync(string username);
+
+        public Task<bool> UserRecoveryByMobileStartAsync(string username);
+
+        public Task<bool> UserRecoveryByEmailStartAsync(string email);
+
+        public Task<bool> UserRecoveryPasswordCompleteAsync(string token, string confirmationCode, string newPassword);
+
+        #endregion
+
+        #region User Agreements
+
         public Task<PagedList<UserAgreementModel>> GetUserAgreementsAsync(UserAgreementsFilter filter);
 
         public Task<List<UserAgreementModelState>> GetUserAgreementStatesAsync(int userId);
@@ -15,6 +37,24 @@ namespace Gizmo.Client
 
         public Task<UpdateResult> RejectUserAgreementForUserAsync(int userAgreementId, int userId);
 
+        #endregion
+
+        #region Registration
+
+        public Task<bool> EmailExistAsync(string email);
+
+        public Task<bool> MobileExistAsync(string mobilePhone);
+
+        public Task<bool> TokenIsValidAsync(int tokenType, string token, string confirmationCode);
+
+        public Task<bool> UsernameExistAsync(string username);
+
+        public Task<UserModelRequiredInfo> GetDefaultUserGroupRequiredInfoAsync();
+
+        #endregion
+
+        #region Products
+
         public Task<PagedList<ProductGroupModel>> GetProductGroupsAsync(ProductGroupsFilter filter);
 
         public Task<PagedList<ProductModel>> GetProductsAsync(ProductsFilter filter);
@@ -22,6 +62,10 @@ namespace Gizmo.Client
         public Task<ProductModel> GetProductByIdAsync(int id, ModelFilterOptions? options = null);
 
         public Task<PagedList<ProductBundledModel>> GetBundledProductsAsync(int id);
+
+        #endregion
+
+        #region Applications
 
         public Task<PagedList<ApplicationGroupModel>> GetApplicationGroupsAsync(ApplicationGroupsFilter filter);
 
@@ -35,8 +79,12 @@ namespace Gizmo.Client
 
         public Task<ApplicationExecutableModelImage> GetApplicationExecutableImageAsync(int id);
 
+        #endregion
+
         public Task<PagedList<PaymentMethodModel>> GetPaymentMethodsAsync(PaymentMethodsFilter filter);
 
-        public Task UserPasswordChange(int userId, string oldPassword, string newPassword);
+        public Task ChangeUserPasswordAsync(int userId, string oldPassword, string newPassword);
+
+        public Task<CreateResult> CreateUserOrderAsync(int userId, OrderCalculateModelOptions calculateOrderOptions);
     }
 }
