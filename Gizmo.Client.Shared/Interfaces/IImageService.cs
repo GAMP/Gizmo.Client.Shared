09567c17334a529c6de47bde6febe39f0cbe2b9f
Oleg Savelos <olegsavelos@gmail.com>
2023-01-27 16:37:56 +0200

    Added image service interface.

---
+++ Gizmo.Client.Shared/Interfaces/IImageService.cs
@@ -0,0 +1,28 @@
+﻿using Gizmo.UI;
+
+namespace Gizmo.Client
+{
+    /// <summary>
+    /// Service responsible of obtaining Gizmo images (product,app and such)
+    /// </summary>
+    public interface IImageService
+    {
+        /// <summary>
+        /// Requests image hash.
+        /// </summary>
+        /// <param name="imageType">Image type.</param>
+        /// <param name="imageId">Image id.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Image hash.</returns>
+        Task<string> ImageHashGetAsync(ImageType imageType, int imageId, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Requests image stream.
+        /// </summary>
+        /// <param name="imageType">Image type.</param>
+        /// <param name="imageId">Image id.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Image stream.</returns>
+        ValueTask<Stream> ImageStreamGetAsync(ImageType imageType, int imageId, CancellationToken cancellationToken);
+    }
+}
