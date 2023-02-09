df70e6cdcdbeb344d11009d4ed6b875f5f79af33
Oleg Savelos <olegsavelos@gmail.com>
2023-02-09 16:14:00 +0200

    Interface updates.

--- Gizmo.Client.Shared/Interfaces/IGizmoClient.cs
+++ Gizmo.Client.Shared/Interfaces/IGizmoClient.cs
@@ -1,4 +1,6 @@
-﻿using Gizmo.Web.Api.Models;
+﻿#nullable enable
+
+using Gizmo.Web.Api.Models;
 
 namespace Gizmo.Client
 {
@@ -7,6 +9,166 @@ namespace Gizmo.Client
     /// </summary>
     public interface IGizmoClient
     {
+        /// <summary>
+        /// Initiates user login.
+        /// </summary>
+        /// <param name="username">Username.</param>
+        /// <param name="password">Optional password.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        public Task<LoginResult> UserLoginAsync(string username,string? password, CancellationToken cancellationToken= default);
+
+        /// <summary>
+        /// Initiates user logout.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns></returns>
+        public Task UserLogoutAsync(CancellationToken cancellationToken= default);
+
+        /// <summary>
+        /// Returns all user agreements based on supplied <paramref name="filter"/>.
+        /// </summary>
+        /// <param name="filter">Filter.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns></returns>
+        public Task<PagedList<UserAgreementModel>> UserAgreementsGetAsync(UserAgreementsFilter filter , CancellationToken cancellationToken= default);
+
+        /// <summary>
+        /// Gets pending user agreements.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        public Task<PagedList<UserAgreementModel>> UserAgreementsPendingGetAsync(UserAgreementsFilter filter , CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Returns all agreement states for current user.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        public Task<List<UserAgreementModelState>> UserAgreementsGetStatesAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Accepts user agreement specified by <paramref name="userAgreementId"/> parameter.
+        /// </summary>
+        /// <param name="userAgreementId">User agreement id.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        public Task<UpdateResult> UserAgreementAcceptAsync(int userAgreementId, CancellationToken cancellationToken =default);
+
+        /// <summary>
+        /// Rejects user agreement specified by <paramref name="userAgreementId"/> parameter.
+        /// </summary>
+        /// <param name="userAgreementId">User agreement id.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        public Task<UpdateResult> UserAgreementRejectAsync(int userAgreementId, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Checks if specified user email exist.
+        /// </summary>
+        /// <param name="email">User email address.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>True or false.</returns>
+        public Task<bool> UserEmailExistAsync(string email, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Checks if specified user mobile exist.
+        /// </summary>
+        /// <param name="mobilePhone">User mobile phone.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>True or false.</returns>
+        public Task<bool> UserMobileExistAsync(string mobilePhone, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Checks if specified user name exist.
+        /// </summary>
+        /// <param name="username">User user name.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>True or false.</returns>
+        public Task<bool> UsernameExistAsync(string username, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Checks if specified token is valid.
+        /// </summary>
+        /// <param name="tokenType">Token type.</param>
+        /// <param name="token">Token value.</param>
+        /// <param name="confirmationCode">Confirmation code.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>True or false.</returns>
+        public Task<bool> TokenIsValidAsync(TokenType tokenType, string token, string confirmationCode, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Gets required user info for default user group.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Required info, the value will be null if no default user group exist.</returns>
+        public Task<UserModelRequiredInfo?> UserGroupDefaultRequiredInfoGetAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Initiates user creation by email address.
+        /// </summary>
+        /// <param name="email">Email address.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Creation result.</returns>
+        public Task<AccountCreationResultModelByEmail> UserCreateByEmailStartAsync(string email, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Initiates user creation by mobile phone.
+        /// </summary>
+        /// <param name="mobilePhone">Mobile phone.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Creation result.</returns>
+        public Task<AccountCreationResultModelByMobilePhone> UserCreateByMobileStartAsync(string mobilePhone, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Completes user creation.
+        /// </summary>
+        /// <param name="user">User profile model.</param>
+        /// <param name="password">User password.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Completion result.</returns>
+        public Task<AccountCreationCompleteResultModel> UserCreateCompleteAsync(UserModelUpdate user, string password, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Completes user creation.
+        /// </summary>
+        /// <param name="token">Token value.</param>
+        /// <param name="user">User profile model.</param>
+        /// <param name="password">User password.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Completion result.</returns>
+        public Task<AccountCreationCompleteResultModelByToken> UserCreateByTokenCompleteAsync(string token, UserModelUpdate user, string password, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Gets user password recovery methods for specified user.
+        /// </summary>
+        /// <param name="userNameEmailOrMobile">User name, mobile or email address.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>User password recovery methods result model.</returns>
+        public Task<UserRecoveryMethodGetResultModel> UserPasswordRecoveryMethodGetAsync(string userNameEmailOrMobile, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Initiates user password recovery by mobile phone.
+        /// </summary>
+        /// <param name="username">User name.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Result.</returns>
+        public Task<PasswordRecoveryStartResultModelByMobile> UserPasswordRecoveryByMobileStartAsync(string username, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Initiates user password recovery by email address.
+        /// </summary>
+        /// <param name="email">User email address.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns></returns>
+        public Task<PasswordRecoveryStartResultModelByEmail> UserPasswordRecoveryByEmailStartAsync(string email, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Completes user password recovery process.
+        /// </summary>
+        /// <param name="token">Token value.</param>
+        /// <param name="confirmationCode">Confirmation code.</param>
+        /// <param name="newPassword">New user password.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Completion result.</returns>
+        public Task<PasswordRecoveryCompleteResultCode> UserPasswordRecoveryCompleteAsync(string token, string confirmationCode, string newPassword, CancellationToken cancellationToken = default);
+
+
         public Task<UserModel> GetUserProfileAsync();
 
         public Task<UpdateResult> UpdateUserProfileAsync(UserModelUpdate user);
@@ -23,47 +185,22 @@ namespace Gizmo.Client
 
         #region Password Recovery
 
-        public Task<bool> UserRecoveryMethodGetAsync(string username);
 
-        public Task<bool> UserRecoveryByMobileStartAsync(string username);
-
-        public Task<bool> UserRecoveryByEmailStartAsync(string email);
-
-        public Task<bool> UserRecoveryPasswordCompleteAsync(string token, string confirmationCode, string newPassword);
 
         #endregion
 
         #region User Agreements
 
-        public Task<PagedList<UserAgreementModel>> GetUserAgreementsAsync(UserAgreementsFilter filter);
 
-        public Task<List<UserAgreementModelState>> GetUserAgreementStatesAsync(int userId);
 
-        public Task<UpdateResult> AcceptUserAgreementForUserAsync(int userAgreementId, int userId);
-
-        public Task<UpdateResult> RejectUserAgreementForUserAsync(int userAgreementId, int userId);
 
         #endregion
 
         #region Registration
 
-        public Task<bool> EmailExistAsync(string email);
 
-        public Task<bool> MobileExistAsync(string mobilePhone);
 
-        public Task<bool> TokenIsValidAsync(int tokenType, string token, string confirmationCode);
 
-        public Task<bool> UsernameExistAsync(string username);
-
-        public Task<UserModelRequiredInfo> GetDefaultUserGroupRequiredInfoAsync();
-
-        public Task<AccountCreationResultModelByEmail> AccountCreationByEmailStartAsync(string email);
-
-        public Task<AccountCreationResultModelByMobilePhone> AccountCreationByMobilePhoneStartAsync(string mobilePhone);
-
-        public Task<AccountCreationCompleteResultModel> AccountCreationCompleteAsync(UserModelUpdate user, string password);
-
-        public Task<AccountCreationCompleteResultModelByToken> AccountCreationByTokenCompleteAsync(string token, UserModelUpdate user, string password);
 
         #endregion
 
@@ -97,7 +234,7 @@ namespace Gizmo.Client
 
         public Task<PagedList<PaymentMethodModel>> GetPaymentMethodsAsync(PaymentMethodsFilter filter);
 
-        public Task ChangeUserPasswordAsync(int userId, string oldPassword, string newPassword);
+        
 
         public Task<CreateResult> CreateUserOrderAsync(int userId, OrderCalculateModelOptions calculateOrderOptions);
     }
