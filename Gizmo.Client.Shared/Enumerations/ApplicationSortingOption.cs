a28cbf30e7046eb240387150a2c6f7034464d90e
klapsis-netprojects <klapsis@netprojects.gr>
2023-03-10 16:32:22 +0200

    Add enum sorting.

---
+++ Gizmo.Client.Shared/Enumerations/ApplicationSortingOption.cs
@@ -0,0 +1,23 @@
+﻿namespace Gizmo.Client
+{
+    public enum ApplicationSortingOption
+    {
+        [Localized("APPLICATION_SORTING_OPTION_POPULARITY")]
+        Popularity = 0,
+        
+        [Localized("APPLICATION_SORTING_OPTION_ADD_DATE")]
+        AddDate = 1,
+
+        [Localized("APPLICATION_SORTING_OPTION_TITLE")]
+        Title = 2,
+
+        [Localized("APPLICATION_SORTING_OPTION_USE")]
+        Use = 3,
+
+        [Localized("APPLICATION_SORTING_OPTION_RATING")]
+        Rating = 4,
+
+        [Localized("APPLICATION_SORTING_OPTION_RELEASE_DATE")]
+        ReleaseDate = 5,
+    }
+}
