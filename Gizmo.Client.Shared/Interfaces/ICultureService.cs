9492e3f2e85d8b977edf192d8d16d82444802c76
Oleg Savelos <olegsavelos@gmail.com>
2023-03-15 16:32:02 +0200

    New services.

---
+++ Gizmo.Client.Shared/Interfaces/ICultureService.cs
@@ -0,0 +1,21 @@
+﻿#nullable enable
+
+using System.Globalization;
+
+namespace Gizmo.Client.Interfaces
+{
+    /// <summary>
+    /// Culture service.
+    /// </summary>
+    /// <remarks>
+    /// Responsible of setting culture based on platform.
+    /// </remarks>
+    public interface ICultureService
+    {
+        /// <summary>
+        /// Sets current UI culture.
+        /// </summary>
+        /// <param name="culture">Culture.</param>
+        Task SetCurrentUICultureAsync(CultureInfo culture);
+    }
+}
---
+++ Gizmo.Client.Shared/Interfaces/IInputLanguageService.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace Gizmo.Client
+{
+    /// <summary>
+    /// Input language service.
+    /// </summary>
+    /// <remarks>
+    /// Provides functionality to list and select sytem wide input language.
+    /// </remarks>
+    public interface IInputLanguageService
+    {
+        /// <summary>
+        /// Gets currently available input languages.
+        /// </summary>
+        IEnumerable<CultureInfo> AvailableInputLanguages { get; }
+
+        /// <summary>
+        /// Gets the current input language.
+        /// </summary>
+        CultureInfo CurrentInputLanguage { get; }
+
+        Task SetCurrentLanguageAsync(CultureInfo inputLanguage);
+    }
+}
