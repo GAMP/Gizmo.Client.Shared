76aa0fa501fc23850b9a92d4713ff421579963e8
Oleg Savelos <olegsavelos@gmail.com>
2023-05-24 10:33:28 +0300

    Added topmost option.

--- Gizmo.Client.Shared/Code/FullScreenEnterOptions.cs
+++ Gizmo.Client.Shared/Code/FullScreenEnterOptions.cs
@@ -6,5 +6,13 @@
     public sealed class FullScreenEnterOptions
     {
         public static readonly FullScreenEnterOptions Default = new();
+
+        /// <summary>
+        /// Indicates that the window should become topmost.
+        /// </summary>
+        /// <remarks>
+        /// Default value is true.
+        /// </remarks>
+        public bool TopMost { get; init; } = true;
     }
 }
