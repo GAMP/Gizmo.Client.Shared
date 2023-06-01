31ecf1b323655dd3e197ea117767eb99ff54df4d
Oleg Savelos <olegsavelos@gmail.com>
2023-06-01 15:17:02 +0300

    Fixes.

--- Gizmo.Client.Shared/Interfaces/IImageService.cs
+++ Gizmo.Client.Shared/Interfaces/IImageService.cs
@@ -15,9 +15,10 @@ namespace Gizmo.Client
         /// <param name="cToken">Cancellation token.</param>
         /// <returns>Image source.</returns>
         /// <remarks>
+        /// Produced image source url can be directly used as img source of html component.<br></br>
         /// The <paramref name="imageId"/> depends on ImageType, in case of executable or application the <paramref name="imageId"/> should be equal to application or executable id.<br></br>
         /// Since single product can have multiple images in case of product it should be equal to product image id instead of product id itself.<br></br>
-        /// The function will only throw <see cref="OperationCanceledException"/>, null is returned in case of an error or <see cref="Stream.Null"/> in case image not set.
+        /// The function will only throw <see cref="OperationCanceledException"/>, null is returned in case of an error or <see cref="string.Empty"/> in case image not set.
         /// </remarks>
         /// <exception cref="OperationCanceledException"></exception>
         ValueTask<string> ImageSourceGetAsync(ImageType imageType, int imageId, CancellationToken cToken);
