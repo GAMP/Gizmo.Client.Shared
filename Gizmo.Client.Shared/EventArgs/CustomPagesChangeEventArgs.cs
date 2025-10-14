2f5ca10b65404db42d281defdf013b77464aa0dd
Аркадий Алфёров <arhe414@gmail.com>
2025-10-14 15:10:07 +0300

    custom pages changes

---
+++ Gizmo.Client.Shared/EventArgs/CustomPagesChangeEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace Gizmo.Client
+{
+    /// <summary>
+    /// Server data change args.
+    /// </summary>
+    public class CustomPagesChangeEventArgs : ModificationEventArgs
+    {
+        public CustomPagesChangeEventArgs(int entityId, ModificationType modificationType) : base(entityId, modificationType)
+        {
+        }
+    }
+}
--- Gizmo.Client.Shared/Interfaces/IGizmoClient.cs
+++ Gizmo.Client.Shared/Interfaces/IGizmoClient.cs
@@ -3,6 +3,7 @@
 using Gizmo.Client.UI;
 using Gizmo.Web.Api.Messaging;
 using Gizmo.Web.Api.Models;
+using Gizmo.Web.Api.Models.Models.API.Request.CustomPage;
 
 namespace Gizmo.Client
 {
@@ -835,5 +836,12 @@ namespace Gizmo.Client
         public Task<bool> AssistanceRequestAnyPendingGetAsync(CancellationToken cancellationToken = default);
 
         public Task<UpdateResult> AssistanceRequestPendingCancelAsync(CancellationToken cancellationToken = default);
+        
+        /// <summary>
+        /// Get custom pages
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>List of <see cref="CustomPageModel"/></returns>
+        public Task<List<CustomPageModel>> CustomPagesGetAsync(CancellationToken cancellationToken = default);
     }
 }
