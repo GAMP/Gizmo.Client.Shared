0eeed3b441e47f5f587e9eefeb99f3555af27e69
klapsis-netprojects <klapsis@netprojects.gr>
2026-02-25 13:27:41 +0200

    Updates.

---
+++ Gizmo.Client.Shared/Code/UrlQRCodeResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Gizmo.Client
+{
+    /// <summary>
+    /// Url qr code generation result.
+    /// </summary>
+    public sealed class UrlQRCodeResult
+    {
+        /// <summary>
+        /// Generated QR code.
+        /// </summary>
+        public string QRCode { get; init; } = null!;
+    }
+}
